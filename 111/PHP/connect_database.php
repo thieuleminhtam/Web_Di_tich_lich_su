74364e689cbda2025187fd52f538a2d339ddf6f9
thieuleminhtam <thieuleminhtam2003@gmail.com>
2025-09-18 22:40:19 +0700

    Delete 111 directory

--- 111/PHP/connect_database.php
+++
@@ -1,8 +0,0 @@
-    <?php
-    define("PG_DB", "DT_lichsu"); // sửa tên database
-    define("PG_HOST","localhost");
-    define("PG_USER","postgres");
-    define("PG_PORT","5432");// 
-    define("PG_PASS", "sharkbodoi0110");// sửa mật khẩu
-    $dbcon=pg_connect("dbname=".PG_DB." user=".PG_USER." password=".PG_PASS." host=".PG_HOST." port=".PG_PORT);
-    ?>
